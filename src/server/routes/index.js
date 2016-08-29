555d731667baf91bc201a4b84f3f976ce708274c
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-08-29 09:33:47 -0600

    init

---
+++ src/server/routes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const indexController = require('../controllers/index');
+
+router.get('/', function (req, res, next) {
+  const renderObject = {};
+  renderObject.title = 'Welcome to Express!';
+  indexController.sum(1, 2, (error, results) => {
+    if (error) return next(error);
+    if (results) {
+      renderObject.sum = results;
+      res.render('index', renderObject);
+    }
+  });
+});
+
+router.get('/test', function(request, response, next) {
+  response.send('test');
+});
+
+module.exports = router;
